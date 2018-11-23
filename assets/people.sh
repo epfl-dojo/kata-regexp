9cb7383ce684fdf6f61ce456a38c01d9e67b6cbd
Nicolas Borboën <ponsfrilus@gmail.com>
2018-11-23 15:19:04 +0100

    [rm] this script is specific to EPFL, not needed.

--- assets/people.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cat anonymized_list.html | grep sciper
-
-echo bonjour
-
- curl https://cadiwww.epfl.ch/listes\?groupe\=epfl-dojo | perl -ne  '/sciper=(.+)\">(.+)<\/a>/ && print "${1} ${2}\n"'
